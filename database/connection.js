1dcda9dea919d87b57aa324520f88410e61c4cd4
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-11-12 11:31:24 +0300

    add

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",       // or your remote DB host
+  host: "dpg-d468nma4d50c73cfjs50-a.oregon-postgres.render.com",
   port: 5432,
-  user: "your_db_user",
-  password: "your_db_password",
-  database: "your_db_name"
+  user: "jordan_yqwo_user",
+  password: "8CkhcyY17ofAF4Rec4h3xXLf6s4Z3odD",
+  database: "jordan_yqwo",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
