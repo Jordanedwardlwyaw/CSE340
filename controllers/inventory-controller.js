9b91ba9e015caa89be2ac47daff06ff95ec401d0
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-11-19 15:56:15 +0300

    add

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -2,9 +2,8 @@ const inventoryModel = require("../models/inventory-model");
 
 exports.showHome = (req, res) => {
   res.render("home", {
-    home: {
-      banner: "/images/home-banner.jpg",
-      message: "Welcome to CSE Motors!"
+    custom: {
+      upgrades: inventoryModel.getCustomUpgrades()
     }
   });
 };
@@ -12,14 +11,23 @@ exports.showHome = (req, res) => {
 exports.showClassification = (req, res, next) => {
   const type = req.params.type;
   const vehicles = inventoryModel.getVehiclesByType(type);
-  if (!vehicles || vehicles.length === 0) return next(new Error("No vehicles found"));
-  res.render("classification", { type, vehicles });
+  if (!vehicles || vehicles.length === 0) {
+    return next(new Error("No vehicles found"));
+  }
+  res.render("classification", {
+    type,
+    vehicles
+  });
 };
 
 exports.showVehicleDetails = (req, res, next) => {
   const vehicle = inventoryModel.getVehicleById(req.params.id);
-  if (!vehicle) return next(new Error("Vehicle not found"));
-  res.render("details", { vehicle });
+  if (!vehicle) {
+    return next(new Error("Vehicle not found"));
+  }
+  res.render("details", {
+    vehicle
+  });
 };
 
 exports.showCustom = (req, res) => {
