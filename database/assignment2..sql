6937d6af319d57ef536d5c97e830faf7e2c619b0
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-12-02 11:20:35 +0300

    add

--- database/assignment2..sql
+++ database/assignment2..sql
@@ -1,20 +1,24 @@
--- Insert new record for Tony Stark
+-- Task 1: Write SQL Statements
+
+-- 1. Insert new record for Tony Stark
 INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
--- Update Tony Stark's account_type to Admin
+
+-- 2. Update Tony Stark's account_type to Admin
 UPDATE account
 SET account_type = 'Admin'
 WHERE account_email = 'tony@starkent.com';
--- Delete Tony Stark's record from the account table
+
+-- 3. Delete Tony Stark's record from the account table
 DELETE FROM account
 WHERE account_email = 'tony@starkent.com';
 
--- Update the GM Hummer description from 'small interiors' to 'a huge interior'
+-- 4. Update the GM Hummer description from 'the small interiors' to 'a huge interior'
 UPDATE inventory
-SET inv_description = REPLACE(inv_description, 'the small interiors', ' a huge interior')
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
--- Select make and model from inventory and classification name from classification table for "Sport" category
+-- 5. Select make and model from inventory and classification name from classification table for "Sport" category
 SELECT 
     inventory.inv_make, 
     inventory.inv_model, 
@@ -28,7 +32,7 @@ ON
 WHERE 
     classification.classification_name = 'Sport';
 
--- Remove '/images/vehicles/vehiclesvehicles' from inv_image and inv_thumbnail paths then replacing with /images/vehicles/
+-- 6. Update image paths to include /vehicles
 UPDATE inventory
 SET 
     inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
